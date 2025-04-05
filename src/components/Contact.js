37e72fabd6e61f774a948af76d850a152fe7d00e
bellapukondaveerendra <bellapukondaveerendra@gmail.com>
2025-04-05 17:22:04 -0500

    changes for user name, align

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,13 @@ import contactImg from "../assets/img/contact-img.jpg";
 import linkedInQRImg from "../assets/img/linkedIN QR code.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
+import {
+  EnvelopeAt,
+  Linkedin,
+  Github,
+  FileEarmarkArrowDown,
+  Telephone,
+} from "react-bootstrap-icons";
 
 export const Contact = () => {
   return (
@@ -35,17 +42,69 @@ export const Contact = () => {
                   <p>
                     You can reach me directly through any of the channels below:
                   </p>
-                  <ul style={{ listStyle: "none", paddingLeft: 0 }}>
-                    <li>
-                      📧 <strong>Email:</strong>{" "}
+                  <ul style={{ listStyle: "none", paddingLeft: 0, margin: 0 }}>
+                    {/* Email */}
+                    <li
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "1rem",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "100px",
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        <span style={{ marginRight: "8px" }}>📧</span>
+                        <strong>Email:</strong>
+                      </div>
                       <a href="mailto:bellapukondaveerendra@gmail.com">
                         bellapukondaveerendra@gmail.com
                       </a>
                     </li>
-                    <br></br>
 
-                    <li>
-                      💼 <strong>LinkedIn:</strong>{" "}
+                    {/* Phone */}
+                    <li
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "1rem",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "100px",
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Telephone size={20} style={{ marginRight: "8px" }} />
+                        <strong>Phone:</strong>
+                      </div>
+                      <a href="tel:+19132184326">+1 (913) 218-4326</a>
+                    </li>
+
+                    {/* LinkedIn */}
+                    <li
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "1rem",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "100px",
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        <span style={{ marginRight: "8px" }}>💼</span>
+                        <strong>LinkedIn:</strong>
+                      </div>
                       <a
                         href="https://www.linkedin.com/in/veerendra-bellapukonda-3a1245235/"
                         target="_blank"
@@ -54,10 +113,25 @@ export const Contact = () => {
                         /veerendra-bellapukonda
                       </a>
                     </li>
-                    <br></br>
 
-                    <li>
-                      💻 <strong>GitHub:</strong>{" "}
+                    {/* GitHub */}
+                    <li
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "1rem",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "100px",
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        <span style={{ marginRight: "8px" }}>💻</span>
+                        <strong>GitHub:</strong>
+                      </div>
                       <a
                         href="https://github.com/bellapukondaveerendra"
                         target="_blank"
@@ -66,27 +140,53 @@ export const Contact = () => {
                         @bellapukondaveerendra
                       </a>
                     </li>
-                    <br></br>
 
-                    <li>
-                      📄 <strong>Resume:</strong>{" "}
-                      <a href="https://drive.google.com/file/d/1nnXRm52KYSvMdrx_hOt6Aazt_b8K4qsh/view">
+                    {/* Resume */}
+                    <li
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "1rem",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "100px",
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        <span style={{ marginRight: "8px" }}>📄</span>
+                        <strong>Resume:</strong>
+                      </div>
+                      <a
+                        href="https://drive.google.com/file/d/1nnXRm52KYSvMdrx_hOt6Aazt_b8K4qsh/view"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
                         Look at my Resume
                       </a>
                     </li>
-                    <br></br>
-                    <li>
-                      <strong>LinkedIn QR:</strong>{" "}
+
+                    {/* QR Code */}
+                    <li
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "1rem",
+                      }}
+                    >
+                      <div style={{ width: "100px" }}>
+                        <strong>QR:</strong>
+                      </div>
                       <img
-                        className={
-                          isVisible ? "animate__animated animate__zoomIn" : ""
-                        }
-                        src={linkedInQRImg} // Replace with your QR image if you have one
-                        alt="QR Code"
+                        src={linkedInQRImg}
+                        alt="LinkedIn QR"
                         style={{
-                          width: "15%",
-                          height: "15%",
-                          borderRadius: "10px",
+                          width: "60px",
+                          height: "60px",
+                          borderRadius: "8px",
+                          marginLeft: "8px",
                         }}
                       />
                     </li>
